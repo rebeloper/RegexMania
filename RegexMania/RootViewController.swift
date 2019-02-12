7638913ccb857ed4dcb8804385687a122dd40829
Alex <therebeloper@gmail.com>
2019-02-12 08:19:16 +0200

    Initial Commit

---
+++ RegexMania/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  RegexMania
+//
+//  Created by Alex Nagy on 12/02/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
